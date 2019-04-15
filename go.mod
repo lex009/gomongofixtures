5b8e83f7c67ff064c95adbe57a118c853e1d010d
Alexey Belyaev <lex@alexbelyaev.com>
2019-04-15 16:01:05 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/lex009/gomongofixtures
+
+go 1.12
+
+require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/go-cmp v0.2.0 // indirect
+	github.com/mongodb/mongo-tools-common v0.0.0
+	github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.0.0
+	golang.org/x/crypto v0.0.0-20190411191339-88737f569e3a // indirect
+	golang.org/x/sync v0.0.0-20190412183630-56d357773e84 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
+)
---
+++ loader_test.go
@@ -0,0 +1,92 @@
+package gomongofixture
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"gopkg.in/mgo.v2/bson"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var uri = func() string {
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		uri = "localhost:27017"
+	}
+
+	return uri
+}
+
+const (
+	testDB  = "test_gomongofixture"
+	testCol = "data"
+)
+
+type elem struct {
+	ID   primitive.ObjectID
+	I    int       `bson:"i"`
+	Time time.Time `bson:"time"`
+}
+
+func TestLoad(t *testing.T) {
+	ctx := context.Background()
+
+	cli, err := mongo.Connect(ctx, options.Client().ApplyURI(uri()))
+	if err != nil {
+		t.Error(err)
+	}
+
+	col := cli.Database(testDB).Collection(testCol)
+
+	defer func() {
+		if _, err := col.DeleteMany(ctx, bson.M{}); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	err = Load(ctx, uri(), Fixture{
+		DB:   testDB,
+		Path: "testdata/data.json",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
+	cur, err := col.Find(ctx, bson.M{}, &options.FindOptions{
+		Sort: bson.M{
+			"i": 1,
+		},
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
+	var elems []elem
+
+	for cur.Next(ctx) {
+		var e elem
+		if err := cur.Decode(&e); err != nil {
+			t.Error(err)
+		}
+
+		elems = append(elems, e)
+	}
+
+	if len(elems) != 10 {
+		t.Error("Invalid count in the database")
+	}
+
+	for i, e := range elems {
+		if e.I != i {
+			t.Error("Invalid number")
+		}
+		if e.Time.IsZero() {
+			t.Error("Time is zero")
+		}
+	}
+}
